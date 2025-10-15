c302d1e6105d6b6e4e4935f1e3ee85a2cf825eaf
Harini Venkatesh <harinisv2103@gmail.com>
2025-10-15 19:04:49 +0530

    Initial Commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,24 @@
+INSERT INTO todo
+(username, description, target_date, done)
+VALUES
+('harini', 'Get AWS Certified', CURRENT_DATE(), false);
+
+INSERT INTO todo
+(username, description, target_date, done)
+VALUES
+('harini', 'Get Azure Certified', CURRENT_DATE(), false);
+
+INSERT INTO todo
+(username, description, target_date, done)
+VALUES
+('harini', 'Get GCP Certified', CURRENT_DATE(), false);
+
+INSERT INTO todo
+(username, description, target_date, done)
+VALUES
+('harini', 'Learn DevOps', CURRENT_DATE(), false);
+
+INSERT INTO todo
+(username, description, target_date, done)
+VALUES
+('admin', 'Learn Java', CURRENT_DATE(), false);
